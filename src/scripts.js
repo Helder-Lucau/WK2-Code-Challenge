09753b5390200922b7824b4a4989bcca67e48cbc
Helder <helderlucau@gmail.com>
2023-06-25 23:21:04 +0300

    display fetched data on browser

--- src/scripts.js
+++ src/scripts.js
@@ -2,29 +2,31 @@
 document.addEventListener("DOMContentLoaded", () => {   
 })
 
-//Fetch animal data from db.json and displays on the browser
-function getData() {
+//Fetch animal data from db.json 
+function fetchData(){
     fetch('http://localhost:3000/characters')
-    .then((res) => res.json())
-    .then((animalData) => {
-
-    let ul = document.querySelector("#animal-list");
-
-    animalData.forEach((characters) => {
-    const list =  document.createElement("li");
-
-    list.textContent = characters.name;
-    ul.append(list)
-        });    
-    });
+    .then(res => res.json())
+    .then(animalData => animalData.forEach(animal =>  printAnimalData(animal)))
 }
-getData();
+fetchData();
 
+//Display fetched data from db.json on the browser
+function printAnimalData(animal) {
 
-function addNewAnimal() {
-    let form = document.getElementById("add-form")
-    form.addEventListener("submit", (e) => {
-        e.preventDefault()
-    });
+    let ul = document.querySelector('#animal-list')
+    
+    let list =  document.createElement("li")
+    list.textContent = animal.name;
+    ul.append(list)    
 }
-addNewAnimal();
+
+
+
+
+// function addNewAnimal() {
+//     let form = document.getElementById("add-form")
+//     form.addEventListener("submit", (e) => {
+//         e.preventDefault()
+//     });
+// }
+// addNewAnimal();
